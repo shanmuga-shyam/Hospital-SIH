24c368d08a64306278c37ea655679c8fb2cfbdf6
Yashwanth1906 <146057554+Yashwanth1906@users.noreply.github.com>
2024-08-14 14:39:46 +0530

    Delete frontend directory

--- frontend/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import './public/Style.css';
-import App from './components/App'
-
-ReactDom.render(<App></App>,document.getElementById("root")
-);
